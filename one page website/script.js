c9000d16489f4bfee5097bad72340d60c5ad469e
zweihs <zweihs@gmail.com>
2022-09-30 09:45:33 +0100

    urban website

---
+++ one page website/script.js
@@ -0,0 +1,7 @@
+function changeColour() {
+    let newColour = document.getElementById('input').value;
+    let element = document.getElementById('change-me');
+
+    element.style.background = newColour
+
+}
